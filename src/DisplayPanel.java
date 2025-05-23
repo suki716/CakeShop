b5dac17155659cfefee09801ccf311ca2a529575
BT_1N3_04 <sample@bths.edu>
2025-05-23 10:54:33 -0400

    basic frame structure

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,18 +1,24 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DisplayPanel extends JPanel implements ActionListener {
     //dialogue options
     private JButton next;
     private JButton cancel;
+    private String order;
+    private String currScreen;
     //play options
     private JButton nextFrame;
     private JButton back; //in case we can do it
     private JButton clear; //trashcan
     private JButton exit;
+    private JButton spin;
     //private JButton recipe;
 
     //batter flavors
@@ -52,129 +58,210 @@ public class DisplayPanel extends JPanel implements ActionListener {
     private Cake userCake;
 
     public DisplayPanel(JFrame frame) {
+        //images
+        bgCounter = loadImage("bgCounter.jpg");
+
+        //logic
         cakeShop = new CakeShop();
+        order = "I want a cake!";
+        currScreen = "order";
         //dialogue options
         next = new JButton("Next");
         next.addActionListener(this);
         add(next);
-        next.setVisible(false);
 
         cancel = new JButton("Cancel");
         cancel.addActionListener(this);
         add(cancel);
-        cancel.setVisible(false);
 
         //play options
         nextFrame = new JButton("NextFrame");
         nextFrame.addActionListener(this);
         add(nextFrame);
-        nextFrame.setVisible(false);
 
         clear = new JButton("Clear");
         clear.addActionListener(this);
         add(clear);
-        clear.setVisible(false);
 
         exit = new JButton("Exit");
         exit.addActionListener(this);
         add(exit);
-        exit.setVisible(false);
+
+        spin = new JButton("spin");
+        spin.addActionListener(this);
+        add(spin);
 
         //batter flavors
         vanilla = new JButton("vanilla");
         vanilla.addActionListener(this);
         add(vanilla);
-        vanilla.setVisible(false);
 
         chocolate = new JButton("chocolate");
         chocolate.addActionListener(this);
         add(chocolate);
-        chocolate.setVisible(false);
 
         strawberry = new JButton("strawberry");
         strawberry.addActionListener(this);
         add(strawberry);
-        strawberry.setVisible(false);
 
         lemon = new JButton("lemon");
         lemon.addActionListener(this);
         add(lemon);
-        lemon.setVisible(false);
 
         matcha = new JButton("matcha");
         matcha.addActionListener(this);
         add(matcha);
-        matcha.setVisible(false);
+
+        cakeLayer = new JButton("cakeLayer");
+        cakeLayer.addActionListener(this);
+        add(cakeLayer);
 
         //frosting
         vanillaFrost = new JButton("vanillaFrost");
         vanillaFrost.addActionListener(this);
         add(vanillaFrost);
-        vanillaFrost.setVisible(false);
 
         chocolateFrost = new JButton("chocolateFrost");
         chocolateFrost.addActionListener(this);
         add(chocolateFrost);
-        chocolateFrost.setVisible(false);
 
         strawberryFrost = new JButton("strawberryFrost");
         strawberryFrost.addActionListener(this);
         add(strawberryFrost);
-        strawberryFrost.setVisible(false);
 
         peppermintFrost = new JButton("peppermintFrost");
         peppermintFrost.addActionListener(this);
         add(peppermintFrost);
-        peppermintFrost.setVisible(false);
 
         peachFrost = new JButton("peachFrost");
         peachFrost.addActionListener(this);
         add(peachFrost);
-        peachFrost.setVisible(false);
 
         //toppings
         candles = new JButton("candles");
         candles.addActionListener(this);
         add(candles);
-        candles.setVisible(false);
 
         strawberries = new JButton("strawberries");
         strawberries.addActionListener(this);
         add(strawberries);
-        strawberries.setVisible(false);
 
         chocolateBar = new JButton("chocolateBar");
         chocolateBar.addActionListener(this);
         add(chocolateBar);
-        chocolateBar.setVisible(false);
 
         apple = new JButton("apple");
         apple.addActionListener(this);
         add(apple);
-        apple.setVisible(false);
 
         cinnamonStick = new JButton("cinnamonStick");
         cinnamonStick.addActionListener(this);
         add(cinnamonStick);
-        cinnamonStick.setVisible(false);
 
         leaves = new JButton("leaves");
         leaves.addActionListener(this);
         add(leaves);
-        leaves.setVisible(false);
     }
 
     public void paintComponent(Graphics g) {
-        next.setLocation(400, 50);
-        cancel.setLocation(400, 100);
-        nextFrame.setLocation(750, 450);
-        exit.setLocation(50, 50);
+        super.paintComponent(g);
+        clear();
+        //background
+        g.drawImage(bgCounter, 0, 0, this);
+        //constant buttons
+        exit.setVisible(true);
+        exit.setLocation(10, 10);
+        if (!currScreen.equals("order")) {
+            nextFrame.setVisible(true);
+            nextFrame.setLocation(675, 425);
+        }
+        if (currScreen.equals("order")) {
+            g.setFont(new Font("Helvetica ", Font.BOLD, 25));
+            g.setColor(Color.BLACK);
+            g.drawString(order, 300, 60);
+            next.setVisible(true);
+            next.setLocation(550, 45);
+            cancel.setVisible(true);
+            cancel.setLocation(550, 75);
+        } else if (currScreen.equals("batter")) {
+            //choose flavor
+            vanilla.setVisible(true);
+            vanilla.setLocation(50, 200);
+            chocolate.setVisible(true);
+            chocolate.setLocation(150, 200);
+            strawberry.setVisible(true);
+            strawberry.setLocation(250, 200);
+            lemon.setVisible(true);
+            lemon.setLocation(350, 200);
+            matcha.setVisible(true);
+            matcha.setLocation(450, 200);
+        } else if (currScreen.equals("layer")) {
+            //adding cake layers
+            cakeLayer.setVisible(true);
+            cakeLayer.setLocation(10, 425);
+            clear.setVisible(true);
+            clear.setLocation(575, 425);
+        } else if (currScreen.equals("frosting")) {
+            //adding frosting;
+            vanillaFrost.setVisible(true);
+            vanillaFrost.setLocation(450, 100);
+            chocolateFrost.setVisible(true);
+            chocolateFrost.setLocation(525, 100);
+            strawberryFrost.setVisible(true);
+            strawberryFrost.setLocation(600,100);
+            peppermintFrost.setVisible(true);
+            peppermintFrost.setLocation(525, 130);
+            peachFrost.setVisible(true);
+            peachFrost.setLocation(600, 130);
+            clear.setVisible(true);
+            clear.setLocation(575, 425);
+        } else if (currScreen.equals("topping")) {
+            //add toppings
+            candles.setVisible(true);
+            candles.setLocation(50, 100);
+            strawberries.setVisible(true);
+            strawberries.setLocation(200, 100);
+            chocolateBar.setVisible(true);
+            chocolateBar.setLocation(50, 150);
+            apple.setVisible(true);
+            apple.setLocation(200, 150);
+            cinnamonStick.setVisible(true);
+            cinnamonStick.setLocation(500, 100);
+            leaves.setVisible(true);
+            leaves.setLocation(600, 100);
+            clear.setVisible(true);
+            clear.setLocation(575, 425);
+        } else if (currScreen.equals("stats")) {
+            //show stats
+            spin.setVisible(true);
+            spin.setLocation(10, 425);
+        } else if (currScreen.equals("spin")) {
+            //spin screen
+            spin.setVisible(true);
+            spin.setLocation(200, 300);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
         JButton casted = (JButton) e.getSource();
-        if (casted == next) {
+        if (currScreen.equals("order") && casted == next) {
+            currScreen = "batter";
+        } else if (currScreen.equals("batter") && casted == nextFrame) {
+            currScreen = "layer";
+        } else if (currScreen.equals("layer") && casted == nextFrame) {
+            currScreen = "frosting";
+        } else if (currScreen.equals("frosting") && casted == nextFrame) {
+            currScreen = "topping";
+        } else if (currScreen.equals("topping") && casted == nextFrame) {
+            currScreen = "stats";
+        } else if (currScreen.equals("stats") && casted == nextFrame) {
+            currScreen = "order";
+        } else if (currScreen.equals("stats") && casted == spin) {
+            currScreen = "spin";
+        } else if (currScreen.equals("spin") && casted == nextFrame) {
+            currScreen = "order";
         }
+        repaint();
 //
 //
 //        //if press start: start the day
@@ -188,24 +275,26 @@ public class DisplayPanel extends JPanel implements ActionListener {
 
     }
 
-    private void Clear() {
+    private void clear() {
         //options
         next.setVisible(false);
         cancel.setVisible(false);
         nextFrame.setVisible(false);
         clear.setVisible(false);
         exit.setVisible(false);
+        spin.setVisible(false);
         //batter
         vanilla.setVisible(false);
         chocolate.setVisible(false);
         strawberry.setVisible(false);
         lemon.setVisible(false);
         matcha.setVisible(false);
+        cakeLayer.setVisible(false);
         //frost
         vanillaFrost.setVisible(false);
         chocolateFrost.setVisible(false);
         strawberryFrost.setVisible(false);
-        peppermintFrost.setVisible(false);
+        peachFrost.setVisible(false);
         peppermintFrost.setVisible(false);
         //toppings
         candles.setVisible(false);
@@ -214,5 +303,14 @@ public class DisplayPanel extends JPanel implements ActionListener {
         apple.setVisible(false);
         cinnamonStick.setVisible(false);
         leaves.setVisible(false);
+    }
+
+    private BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(new File(path));
+        } catch (IOException e) {
+            System.out.println("not loaded in");
+            return null;
+        }
     }
 }
